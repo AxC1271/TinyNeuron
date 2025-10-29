8b450f464c38619c978fc9109b141e1ebc9e2a64
Andrew Chen <109437047+AxC1271@users.noreply.github.com>
2025-10-28 21:50:08 -0400

    Update tb.v

--- src/tb.v
+++ src/tb.v
@@ -1,1 +1,14 @@
+/*
+ * Copyright (c) 2024 Andrew Chen
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
+
+// Simplified Pong for TinyTapeout
+// Testbench to verify behavior
+// 640x480 @ 60Hz VGA
+// Single paddle + ball
+
+module tb();
+
+endmodule
