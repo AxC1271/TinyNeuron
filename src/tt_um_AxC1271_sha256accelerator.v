df33bfbc51eeda9fefb36ffb3e944a9d19b95820
Andrew Chen <109437047+AxC1271@users.noreply.github.com>
2025-10-28 20:56:04 -0400

    Update tt_um_AxC1271_sha256accelerator.v

--- src/tt_um_AxC1271_sha256accelerator.v
+++ src/tt_um_AxC1271_sha256accelerator.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_sha256accelerator (
+module tt_um_axc1271_tinyneuron (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
